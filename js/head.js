9a7a1247903f2b599d68aa9ced740c84e33b8a59
My-fo-mercy <961911175@qq.com>
2019-03-27 11:57:54 +0800

    v0.0.4

---
+++ js/head.js
@@ -0,0 +1,10 @@
+// $(function() {
+//     $('.user-entry').on({
+//         mouseover: function() {
+//             $(this).siblings('.head').css({ "visibility": "inherit" })
+//         }
+//     }, '.lista1,.lista2,.lista3').mouseout(function() {
+//         console.log($(this))
+//         $(this).children('.head').css({ "visibility": "hidden" })
+//     })
+// })
